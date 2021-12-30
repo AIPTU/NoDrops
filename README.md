0334f39069b9a012feaf699de2ac45db860cc339
AIPTU <anangp808@gmail.com>
2021-12-30 22:47:44 +0700

    Initialize

--- README.md
+++ README.md
@@ -1,29 +1,36 @@
 # NoDrops
 
 [![Discord](https://img.shields.io/discord/830063409000087612?color=7389D8&label=discord)](https://discord.com/invite/EggNF9hvGv)
-[![GitHub License](https://img.shields.io/github/license/AIPTU/NoDrops.svg)](https://github.com/AIPTU/NoDrops/blob/master/LICENSE)
 
-A PocketMine-MP plugin to prohibit players from dropping their items
+A PocketMine-MP plugin to prohibit players from dropping their items.
 
-# Config
+# Features
+
+- Permission bypass.
+- Supports `&` as formatting codes.
+- Custom item that can be dropped.
+- Per world support.
+- Lightweight and open source ❤️
+
+# Default Config
 ```yaml
 ---
 # Do not change this (Only for internal use)!
-config-version: 1
+config-version: 1.0
 
-# Message used when canceling a player's drop.
+# Message used when canceling a player dropping an item.
 # Use "§" or "&" to color the message.
 message: "&cYou can't drop your items here"
 
 items:
-  # List of items that can't be dropped.
+  # List of items that can be dropped.
   # If the item has meta, you can use the format "minecraft:id:meta".
   list:
-    - "minecraft:log"
+    - "minecraft:oak_log"
     - "minecraft:diamond_ore"
     - "minecraft:apple"
     - "minecraft:diamond_pickaxe"
-    - "minecraft:sapling"
+    - "minecraft:oak_sapling"
 
 worlds:
   # The mode can be either "blacklist" or "whitelist".
@@ -31,7 +38,7 @@ worlds:
   # Whitelist mode will only allow player's drop according to predefined world folder names and will cancel player's drop in all worlds.
   mode: "blacklist"
   # List of world folder names to blacklist/whitelist (depending on the mode set above).
-  # Set it to [] if you want to allow player's drop in all worlds.
+  # Leave it empty if you want to allow player's drop in all worlds.
   list:
     - "world"
 ...
@@ -40,38 +47,17 @@ worlds:
 
 # How to Install
 
-1. Download the plugin from [here](https://poggit.pmmp.io/ci/AIPTU/NoDrops/NoDrops)
+1. Download the plugin from [here](https://poggit.pmmp.io/ci/AIPTU/NoDrops/NoDrops).
 2. Put the `NoDrops.phar` file into the `plugins` folder.
 3. Restart the server.
 4. Done!
 
+# Upcoming Features
+
+- Currently none planned. You can contribute or suggest for new features.
+
 # Additional Notes
 
-- If you find bugs or want to give suggestions, please visit [here](https://github.com/AIPTU/NoDrops/issues)
+- If you find bugs or want to give suggestions, please visit [here](https://github.com/AIPTU/NoDrops/issues).
+- We accept any contributions! If you want to contribute please make a pull request in [here](https://github.com/AIPTU/NoDrops/pulls).
 - Icons made from [www.flaticon.com](https://www.flaticon.com)
-
-# License
-
-```
-MIT License
-
-Copyright (c) 2021 AIPTU
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-```
--- src/NoDrops.php
+++ src/NoDrops.php
@@ -1,26 +1,58 @@
 <?php
 
+/*
+ *
+ * Copyright (c) 2021 AIPTU
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
 declare(strict_types=1);
 
 namespace aiptu\nodrops;
 
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerDropItemEvent;
+use pocketmine\item\Item;
 use pocketmine\item\LegacyStringToItemParser;
 use pocketmine\item\LegacyStringToItemParserException;
+use pocketmine\item\StringToItemParser;
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\TextFormat;
 use pocketmine\world\World;
-use function gettype;
+use function explode;
 use function in_array;
 use function rename;
+use function str_replace;
+use function trim;
 
 final class NoDrops extends PluginBase implements Listener
 {
+	private const CONFIG_VERSION = 1.0;
+
 	private const MODE_BLACKLIST = 0;
 	private const MODE_WHITELIST = 1;
 
 	private int $mode;
+
+	private ConfigProperty $configProperty;
 
 	public function onEnable(): void
 	{
@@ -38,15 +70,11 @@ final class NoDrops extends PluginBase implements Listener
 		$player = $event->getPlayer();
 		$item = $event->getItem();
 
-		if (!isset($this->getConfig()->getAll()['items']['list'])) {
-			return;
-		}
-
-		foreach ($this->getConfig()->getAll()['items']['list'] as $value) {
-			$itemDrops = LegacyStringToItemParser::getInstance()->parse($value);
+		foreach ($this->getConfigProperty()->getPropertyArray('items.list', []) as $value) {
+			$itemDrops = $this->checkItem($value);
 
 			if ($item->getId() === $itemDrops->getId() && $item->getMeta() === $itemDrops->getMeta()) {
-				if (!$this->checkWorlds($player->getWorld())) {
+				if (!$this->checkWorld($player->getWorld())) {
 					return;
 				}
 
@@ -54,17 +82,22 @@ final class NoDrops extends PluginBase implements Listener
 					return;
 				}
 
-				$player->sendPopup(TextFormat::colorize($this->getConfig()->get('message', "&cYou can't drop your items here")));
+				$player->sendPopup(TextFormat::colorize($this->getConfigProperty()->getPropertyString('message', "&cYou can't drop your items here")));
 				$event->cancel();
 			}
 		}
+	}
+
+	public function getConfigProperty(): ConfigProperty
+	{
+		return $this->configProperty;
 	}
 
 	private function checkConfig(): void
 	{
 		$this->saveDefaultConfig();
 
-		if ($this->getConfig()->get('config-version', 1) !== 1) {
+		if (!$this->getConfig()->exists('config-version') || ($this->getConfig()->get('config-version', self::CONFIG_VERSION) !== self::CONFIG_VERSION)) {
 			$this->getLogger()->notice('Your configuration file is outdated, updating the config.yml...');
 			$this->getLogger()->notice('The old configuration file can be found at config.old.yml');
 
@@ -73,40 +106,38 @@ final class NoDrops extends PluginBase implements Listener
 			$this->reloadConfig();
 		}
 
-		foreach ([
-			'message' => 'string',
-			'worlds.list' => 'array',
-		] as $option => $expectedType) {
-			if (($type = gettype($this->getConfig()->getNested($option))) !== $expectedType) {
-				throw new \TypeError("Config error: Option ({$option}) must be of type {$expectedType}, {$type} was given");
-			}
+		$this->configProperty = new ConfigProperty($this->getConfig());
+
+		foreach ($this->getConfigProperty()->getPropertyArray('items.list', []) as $value) {
+			$this->checkItem($value);
 		}
 
-		if (!isset($this->getConfig()->getAll()['items']['list'])) {
-			return;
-		}
-
-		foreach ($this->getConfig()->getAll()['items']['list'] as $value) {
-			try {
-				$item = LegacyStringToItemParser::getInstance()->parse($value);
-			} catch (LegacyStringToItemParserException $e) {
-				throw $e;
-			}
-		}
-
-		match ($this->getConfig()->getNested('worlds.mode')) {
+		match ($this->getConfigProperty()->getPropertyString('worlds.mode', 'blacklist')) {
 			'blacklist' => $this->mode = self::MODE_BLACKLIST,
 			'whitelist' => $this->mode = self::MODE_WHITELIST,
 			default => throw new \InvalidArgumentException('Invalid mode selected, must be either "blacklist" or "whitelist"!'),
 		};
 	}
 
-	private function checkWorlds(World $world): bool
+	private function checkItem(string $string): Item
 	{
-		if ($this->mode === self::MODE_BLACKLIST) {
-			return !(in_array($world->getFolderName(), $this->getConfig()->getAll()['worlds']['list'], true));
+		try {
+			$item = LegacyStringToItemParser::getInstance()->parse($string);
+		} catch (LegacyStringToItemParserException $e) {
+			if (($item = StringToItemParser::getInstance()->parse(explode(':', str_replace([' ', 'minecraft:'], ['_', ''], trim($string)))[0])) === null) {
+				throw $e;
+			}
 		}
 
-		return in_array($world->getFolderName(), $this->getConfig()->getAll()['worlds']['list'], true);
+		return $item;
+	}
+
+	private function checkWorld(World $world): bool
+	{
+		if ($this->mode === self::MODE_BLACKLIST) {
+			return !(in_array($world->getFolderName(), $this->getConfigProperty()->getPropertyArray('worlds.list', []), true));
+		}
+
+		return in_array($world->getFolderName(), $this->getConfigProperty()->getPropertyArray('worlds.list', []), true);
 	}
 }
